9d52d7d8eeede0006229670c85f241d038028f9d
F227 <user@F227-f.(none)>
2014-05-19 11:45:54 +0800

    initial commit with devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  def new
+		@user = User.new
+  end
+
+  def create
+		@user = User.new
+		@user.username = params[:user][:username]
+		@user.password = params[:user][:password]
+		@user.save!
+		flash[:notice] = "New user created."
+		redirect_to :user
+  end
+
+	def index
+		@users = User.all	
+	end
+
+	def show
+		@user = User.find(params[:id])	
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.username = params[:user][:username]
+		@user.password = params[:user][:password]
+		@user.save!
+		flash[:notice] = "#{@user.id} is updated."
+		render :index
+	end	
+
+end
